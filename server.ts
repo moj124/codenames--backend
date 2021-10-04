600dc2a7113737bdb0f4117e0aaca8b9fbad9b90
michael jones <michael.jowen.jones@gmail.com>
2021-10-04 12:37:55 +0100

    added new session generation endpoint

--- server.ts
+++ server.ts
@@ -33,12 +33,27 @@ app.get("/", async (req, res) => {
   res.json(dbres.rows);
 });
 
-app.get("/generate", async (req, res) => {
+app.get("/generateWords", async (req, res) => {
   try {
     const dbres = await client.query('select * from words');
     res.status(201).json({
       status: "success",
       data: shuffle(generateWords(dbres.rows,false))
+    });
+
+  } catch (err) {
+    console.error(err.message);
+  }
+  
+
+})
+
+app.get("/generateSession", async (req, res) => {
+  try {
+    const dbres = await client.query('select gen_random_uuid()');
+    res.status(201).json({
+      status: "success",
+      session: dbres.rows
     });
 
   } catch (err) {
