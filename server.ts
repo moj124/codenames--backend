0196c77b3d6c98ff6011a5f504351c1b510a1e27
michael jones <michael.jowen.jones@gmail.com>
2021-10-04 16:19:28 +0100

    checking update

--- server.ts
+++ server.ts
@@ -80,6 +80,7 @@ app.put("/game/:session", async (req, res) => {
   try {
     const { session } = req.params;
     const { boardState } = req.body;
+    console.log(boardState)
     const text = 'UPDATE session_data SET ishidden = $1 WHERE session = $2 and word_id = $3'
     boardState.map(async (element: Word) => await client.query(text,[element.ishidden,session,element.word_id]))
 
