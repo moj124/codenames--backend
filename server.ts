60fe368a9d2b1487280e903de1e6af6bf142479e
michael jones <michael.jowen.jones@gmail.com>
2021-10-07 10:38:14 +0100

    turn

--- server.ts
+++ server.ts
@@ -36,7 +36,7 @@ app.get("/game/:session", async (req, res) => {
 
     const dbres1 = await client.query('select turn from session where session = $1',[session])
 
-    const turn = !dbres1.rows[0]
+    const turn = dbres1.rows[0]
 
     res.status(201).json({
       status: "Get is working",
@@ -118,7 +118,7 @@ app.put("/game/:session", async (req, res) => {
   try {
     const { session } = req.params;
     let text = 'UPDATE session_data SET ishidden = $1 WHERE session = $2 and word_id = $3'
-    console.log(req.body.turn, req.body)
+    // console.log(req.body.turn, req.body)
     if(req.body.data){
       req.body.data.map(async (element: Word) => await client.query(text,[false,session,element.word_id]))
     }
